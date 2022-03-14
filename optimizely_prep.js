1923737e6c9c277af421f87e761931036b30a736
smllygrl <hodgsonatalie@gmail.com>
2022-03-14 12:21:00 +1100

    added headers to dad joke api call and now can access json data

--- optimizely_prep.js
+++ optimizely_prep.js
@@ -1,5 +1,8 @@
-let headers = new Headers({
-  "User-Agent": "",
+// Request headers
+
+let myHeaders = new Headers({
+  "User-Agent": "https://github.com/smllygrl/optimizelyPrep",
+  Accept: "application/json",
 });
 
 // KANYE
@@ -21,27 +24,24 @@ const accessKanye = async () => {
   kanyeDiv.appendChild(kanyePara);
 };
 
-// POEMS
-const poemBtn = document.getElementById("poems");
-
-poemBtn.addEventListener("click", () => {
-  console.log("Poem button pressed");
-});
-
 // DAD JOKES
 const dadJokeBtn = document.getElementById("dadjoke");
 const dadJokeDiv = document.getElementById("dadjokeDOM");
-const DADJOKE_URL = "https://icanhazdadjoke.com";
+const DADJOKE_URL = "https://icanhazdadjoke.com/";
 dadJokeBtn.addEventListener("click", () => {
   console.log("Dad joke button pressed");
   accessDadJoke();
 });
 
 const accessDadJoke = async () => {
-  const dadJokeRaw = await fetch(DADJOKE_URL);
-  console.log(dadJokeRaw);
+  const dadJokeRaw = await fetch(`${DADJOKE_URL}`, {
+    headers: myHeaders,
+  });
   const dadJokeResponse = await dadJokeRaw.json();
-  console.log(dadJokeResponse);
+  const dadjokePara = document.createElement("p");
+  const dadjokeText = document.createTextNode(`${dadJokeResponse.joke}`);
+  dadjokePara.appendChild(dadjokeText);
+  dadJokeDiv.appendChild(dadjokePara);
 };
 
 // SIMPLE TEXT

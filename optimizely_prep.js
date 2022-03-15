6837fdbd4b6ec61ef59ed8609fb1ce169307bb3d
smllygrl <hodgsonatalie@gmail.com>
2022-03-15 11:39:31 +1100

    all functionality is established

--- optimizely_prep.js
+++ optimizely_prep.js
@@ -6,9 +6,14 @@ let myHeaders = new Headers({
 });
 
 // KANYE
-const kanyeBtn = document.getElementById("kanye");
+const kanyeBtn = document.getElementById("kanyeBtn");
+const kanyeClearBtn = document.getElementById("kanyeBtn__clear");
 const kanyeDiv = document.getElementById("kanyeDOM");
 const KANYE_URL = "https://api.kanye.rest";
+
+kanyeClearBtn.addEventListener("click", () => {
+  clear(kanyeDiv);
+});
 
 kanyeBtn.addEventListener("click", () => {
   console.log("Kanye button pressed");
@@ -16,36 +21,71 @@ kanyeBtn.addEventListener("click", () => {
 });
 
 const accessKanye = async () => {
+  clear(kanyeDiv);
   const kanyeRaw = await fetch(`${KANYE_URL}`);
   const kanyeResponse = await kanyeRaw.json();
-  const kanyePara = document.createElement("p");
-  const kanyeText = document.createTextNode(`${kanyeResponse.quote}`);
-  kanyePara.appendChild(kanyeText);
-  kanyeDiv.appendChild(kanyePara);
+  const kanyeToRender = `${kanyeResponse.quote}`;
+  append("p", kanyeToRender, kanyeDiv);
 };
 
+// const clearKanye = ()
+
 // DAD JOKES
-const dadJokeBtn = document.getElementById("dadjoke");
+const dadJokeBtn = document.getElementById("dadjokeBtn");
 const dadJokeDiv = document.getElementById("dadjokeDOM");
+const dadJokeClearBtn = document.getElementById("dadjokeBtn__clear");
 const DADJOKE_URL = "https://icanhazdadjoke.com/";
+
+dadJokeClearBtn.addEventListener("click", () => {
+  clear(dadJokeDiv);
+});
+
 dadJokeBtn.addEventListener("click", () => {
   console.log("Dad joke button pressed");
   accessDadJoke();
 });
 
 const accessDadJoke = async () => {
+  clear(dadJokeDiv);
   const dadJokeRaw = await fetch(`${DADJOKE_URL}`, {
     headers: myHeaders,
   });
   const dadJokeResponse = await dadJokeRaw.json();
-  const dadjokePara = document.createElement("p");
-  const dadjokeText = document.createTextNode(`${dadJokeResponse.joke}`);
-  dadjokePara.appendChild(dadjokeText);
-  dadJokeDiv.appendChild(dadjokePara);
+  const dadJokeRender = `${dadJokeResponse.joke}`;
+  append("p", dadJokeRender, dadJokeDiv);
 };
 
 // SIMPLE TEXT
 const textBtn = document.getElementById("textSubmit");
+const textBoxDiv = document.getElementById("textboxDOM");
+const textInput = document.getElementById("textInput");
+const textClearBtn = document.getElementById("textDelete");
+
+textClearBtn.addEventListener("click", () => {
+  clear(textBoxDiv);
+});
+
 textBtn.addEventListener("click", () => {
   console.log("Text button pressed");
+  accessText();
 });
+
+const accessText = () => {
+  clear(textBoxDiv);
+  const textToRender = document.querySelector("input").value;
+  console.log(textToRender);
+  append("p", textToRender, textBoxDiv);
+  textInput.value = "";
+};
+
+// HELPER FUNCITON
+const append = (element, nodeContent, parent) => {
+  const newElement = document.createElement(element);
+  const elementContent = document.createTextNode(nodeContent);
+  newElement.appendChild(elementContent);
+  parent.appendChild(newElement);
+};
+
+const clear = (divID) => {
+  divID.innerHTML = "";
+};
